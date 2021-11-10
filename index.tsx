bbaf85c8e4880222fcc84bdff4a4d21a03f6e94e
menzen <michi.menzel@gmail.com>
2021-11-10 13:36:28 +0100

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,8 +2,43 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+type Animal = {
+  name: string;
+};
+
+type AnimalColor = {
+  color: string;
+};
+
+type AnimalAge = {
+  age: number;
+};
+
+type FlyingAnimal = Animal &
+  AnimalColor &
+  AnimalAge & {
+    canFly: boolean;
+  };
+
+const animal: FlyingAnimal = {
+  name: 'Nachtigal',
+  color: 'gray',
+  age: 12,
+  canFly: true,
+};
+
 const App = () => {
-  return <div>REACT - TS PLAYGROUND</div>;
+  const { name, color, age, canFly } = animal;
+
+  return (
+    <div>
+      <h1>REACT - TS - PLAYGROUND</h1>
+      <div>
+        {name} is {color} and {age} years old and{' '}
+        {canFly ? 'can fly' : 'can not fly'} :-)
+      </div>
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('root'));

37c896fc43b4eb0526449835958ff8266d0926a9
menzen <michi.menzel@gmail.com>
2021-11-10 13:22:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-8jlgku
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-8jlgku)
---
+++ index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => {
+  return <div>REACT - TS PLAYGROUND</div>;
+};
+
+render(<App />, document.getElementById('root'));
